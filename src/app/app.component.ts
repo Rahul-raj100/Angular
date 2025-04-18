95cd84116c9dabfb7042ce66e6c1b11f530f2852
Rahul Raj <tomarraj937@gmail.com>
2025-04-19 00:43:49 +0530

    Second Commit after HTML and CSS and created login page and header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,49 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DemoComponent } from './demo/demo.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [LoginComponent, NavbarComponent, DemoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'first-app';
+  x = 20;
+  y = 30;
+
+  onClick(){
+    alert("function called");
+    this.onDoubleClick();
+  }
+
+  onDoubleClick(){
+    console.log("other function");
+  }
+
+  count = 0
+
+  handleCounter(para:string){
+    if(para == "plus"){
+      this.count = this.count + 1
+    }
+    else if (para == "minus"){
+      this.count = this.count - 1
+    }
+    else {
+      this.count = 0
+    }
+  }
+
+  handleEvent(event:any){
+    console.log("Function called", event.type);
+    // console.log("Function called", event.class);
+  }
+
+  getInput(event:any){
+    console.log("event called", event.target.value);
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' }, //default route
+    { path: 'login', component: LoginComponent },
+
+];
